98f2bec5f81957972c2d29fb54642b2efc4ca92f
Evgeniy Baydukalov <baydukalov3@mail.ru>
2017-11-12 23:29:44 +0500

    password

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,17 @@
+import getpass
+import passwordmeter
 
 
-def get_password_strength(password):
-    pass
+def get_password_strength(password_strength):
+    password_grade, suggestions = password_strength
+    password_grade = int(password_grade * 10)
+    if password_grade > 1:
+        print("Password strength {:d} out of 10".format(password_grade))
+    else:
+        print ('Your password is too weak.')
 
 
 if __name__ == '__main__':
-    pass
+    user_password = getpass.getpass()
+    password_strength = passwordmeter.test(user_password)
+    get_password_strength(password_strength)
